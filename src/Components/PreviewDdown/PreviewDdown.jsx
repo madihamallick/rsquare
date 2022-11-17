bb9279086633b1f4a327c11588d69eeb6f1d29fc
Madiha <mallickmadiha9031@gmail.com>
2022-11-17 14:30:11 +0530

    changes

--- src/Components/PreviewDdown/PreviewDdown.jsx
+++ src/Components/PreviewDdown/PreviewDdown.jsx
@@ -1,50 +1,53 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useNavigate } from "react-router-dom";
 
 // getting id from cookies
-const user = document.cookie.split('=')[1];
+const user = document.cookie.split("=")[1];
 const userObj = user && JSON.parse(user);
 console.log(userObj?.id);
 
-
-
 const thumbsContainer = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  marginTop: 16
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  marginTop: 16,
 };
 
 const thumb = {
-  display: 'inline-flex',
+  display: "inline-flex",
   borderRadius: 2,
-  border: '1px solid #eaeaea',
+  border: "1px solid #eaeaea",
   marginBottom: 8,
   marginRight: 8,
   width: 100,
   height: 100,
   padding: 4,
-  boxSizing: 'border-box'
+  boxSizing: "border-box",
 };
 
 const thumbInner = {
-  display: 'flex',
+  display: "flex",
   minWidth: 0,
-  overflow: 'hidden'
+  overflow: "hidden",
 };
 
 const img = {
-  display: 'block',
-  width: 'auto',
-  height: '100%'
+  display: "block",
+  width: "auto",
+  height: "100%",
 };
-
 
 let base64String = "";
 
-function imageUploaded(files, setLoading, navigate, setFileUpload, setLoadingPercentage) {
+function imageUploaded(
+  files,
+  setLoading,
+  navigate,
+  setFileUpload,
+  setLoadingPercentage
+) {
   for (let i = 0; i < files.length; i++) {
     setLoading(true);
     const reader = new FileReader();
@@ -62,62 +65,70 @@ function imageUploaded(files, setLoading, navigate, setFileUpload, setLoadingPer
         image: base64String,
         userId: userObj.id,
         fileName: fileName,
-        fileType: fileType
-      }
-      axios.post(`https://rsquare-auth.herokuapp.com/images/${userObj.id}`, data)
-        .then(res => {
+        fileType: fileType,
+      };
+      axios
+        .post(`http://localhost:5000/images/${userObj.id}`, data)
+        .then((res) => {
           console.log(res);
           setLoading(false);
-          navigate("/")
-          window.location.reload()
+          navigate("/");
+          window.location.reload();
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
           setLoading(false);
-        })
-
+        });
     };
     base64String = "";
-    console.log("done")
+    console.log("done");
 
     setFileUpload(i + 1);
 
     // calculating percentage in basis of total files
-    const percentage = Math.round((i + 1) / files.length * 100);
+    const percentage = Math.round(((i + 1) / files.length) * 100);
     console.log(percentage);
     setLoadingPercentage(percentage);
 
-
     reader.onerror = (error) => {
-      console.log('Error: ', error);
-    }
+      console.log("Error: ", error);
+    };
   }
 }
-
-
-
 
 export function Previews(props) {
   const [acceptImage, setAcceptImage] = useState([]);
   const [files, setFiles] = useState([]);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [loadingPercentage, setLoadingPercentage] = useState(0);
   const [fileUpload, setFileUpload] = useState(false);
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
-      'image/*': []
+      // accept all image types jpeg, png, gif, svg, webp, jpg etc
+      image: [
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "image/svg+xml",
+        "image/webp",
+        "image/jpg",
+      ],
     },
-    onDrop: acceptedFiles => {
-      setFiles(acceptedFiles.map(file => Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      })));
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
       setAcceptImage(acceptedFiles);
       // imageUploaded(acceptedFiles);
-    }
+    },
   });
 
-  const thumbs = files.map(file => (
+  const thumbs = files.map((file) => (
     <div style={thumb} key={file.name}>
       <div style={thumbInner}>
         <img
@@ -125,7 +136,9 @@ export function Previews(props) {
           style={img}
           alt="preview"
           // Revoke data uri after image is loaded
-          onLoad={() => { URL.revokeObjectURL(file.preview) }}
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
         />
       </div>
     </div>
@@ -133,7 +146,7 @@ export function Previews(props) {
 
   useEffect(() => {
     // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
-    return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
   }, [files]);
 
   return (
@@ -141,20 +154,18 @@ export function Previews(props) {
       <section
         className="container mb-5 mx-auto  cursor-pointer border-dotted border-2 py-8 flex justify-center flex-col items-center"
         style={{
-          transition: 'all 0.5s ease-in-out',
+          transition: "all 0.5s ease-in-out",
         }}
       >
         {files.length > 0 ? null : (
-          <div {...getRootProps({ className: 'dropzone' })}>
+          <div {...getRootProps({ className: "dropzone" })}>
             <input {...getInputProps()} />
-            <p className='text-center'>Drag Files Here</p>
+            <p className="text-center">Drag Files Here</p>
             <br />
-            <p className='text-center'>Or</p>
+            <p className="text-center">Or</p>
             <br />
-            <button className=' hover:bg-Bluish hover:text-white  text-Bluish py-2 px-6 rounded-md border-2 border-Bluish'>
-              <span className='flex flex-row'>
-                Browse
-              </span>
+            <button className=" hover:bg-Bluish hover:text-white  text-Bluish py-2 px-6 rounded-md border-2 border-Bluish">
+              <span className="flex flex-row">Browse</span>
             </button>
           </div>
         )}
@@ -167,49 +178,49 @@ export function Previews(props) {
       {loading && (
         <>
           <div class="w-full bg-grey400 shadow-sm rounded-full h-2.5 mb-2 dark:bg-grey700">
-            <div class="bg-Bluish h-2.5 rounded-full dark:bg-Bluish" style={{ width: { loadingPercentage } }}></div>
+            <div
+              class="bg-Bluish h-2.5 rounded-full dark:bg-Bluish"
+              style={{ width: { loadingPercentage } }}
+            ></div>
           </div>
           <div className="flex justify-between">
             <div className="text-center text-sm text-grey700">
-              {fileUpload} /   {files.length}   uploaded
+              {fileUpload} / {files.length} uploaded
             </div>
             {/* time remaining */}
             <div className="flex flex-col mb-2 items-center">
               <div className="text-grey700 text-sm text-center">
-                Time Remaining:
-
-
-                00:00:00
-
+                Time Remaining: 00:00:00
               </div>
             </div>
           </div>
         </>
       )}
 
-
       {/* Add more and upload button to the right */}
-      <div className='flex flex-row justify-end '>
-        <div {...getRootProps({ className: 'dropzone' })}>
+      <div className="flex flex-row justify-end ">
+        <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
-          <button className=' hover:bg-Bluish hover:text-white  text-Bluish py-2 px-6 rounded-md border-2 border-Bluish'>
-            <span className='flex flex-row'>
-              Add More
-            </span>
+          <button className=" hover:bg-Bluish hover:text-white  text-Bluish py-2 px-6 rounded-md border-2 border-Bluish">
+            <span className="flex flex-row">Add More</span>
           </button>
         </div>
-        <button className=' bg-Bluish text-white py-2 px-6 rounded-md border-2 border-Bluish ml-5'
+        <button
+          className=" bg-Bluish text-white py-2 px-6 rounded-md border-2 border-Bluish ml-5"
           onClick={() => {
-            imageUploaded(acceptImage, setLoading, navigate, setFileUpload, setLoadingPercentage);
+            imageUploaded(
+              acceptImage,
+              setLoading,
+              navigate,
+              setFileUpload,
+              setLoadingPercentage
+            );
             // navigate("/")
           }}
         >
-          <span className='flex flex-row'>
-            Upload
-          </span>
+          <span className="flex flex-row">Upload</span>
         </button>
       </div>
-
     </>
   );
 }
